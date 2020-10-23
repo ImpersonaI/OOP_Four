eb0b6233be6977fa73ff703015f110565299b3e6
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-10-23 15:53:26 +0300

    Add files via upload

---
+++ OOP_four — копия (2)/OOP_four/Machine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_four
+{
+    class Machine
+    {
+        public string Name { get; set; }
+        public double Speed { get; set; }
+        public double Weight { get; set; }
+
+        /*public Machine (string Name, double Speed, double Weight)
+        {
+            this.Name = Name;
+            this.Speed = Speed;
+            this.Weight = Weight;
+            Console.Write(this.Name + " " + this.Speed +" "+ this.Weight);
+        }
+       */
+    }
+}
